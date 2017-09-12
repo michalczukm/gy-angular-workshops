709cf910c823a45c90aa0c214d9d9b2a41d1a846
Michal Michalczuk <michalczukm@gmail.com>
2017-09-12 23:15:28 +0200

    add npm dep to `typescript-examples`

--- typescript-examples/dist/app.js
+++ typescript-examples/dist/app.js
@@ -1,5 +1,6 @@
 "use strict";
-var notes_service_1 = require('./notes.service');
+exports.__esModule = true;
+var notes_service_1 = require("./notes.service");
 var service = new notes_service_1.NotesService();
 service.add('first note');
 service.add('goyello note ... the second one');
